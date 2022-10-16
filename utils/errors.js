7a7d54a6012cb241bb3b71f972dc2877777cf63e
Roman Elensky <warrior.394@yandex.ru>
2022-10-17 02:35:22 +0400

    2

--- utils/errors.js
+++ utils/errors.js
@@ -1,7 +1,7 @@
-const ERROR_CODE = 400;
-const NOT_FOUND = 404;
-const ERROR_DEFAULT = 500;
+const ErrorCode = 400;
+const NotFound = 404;
+const DeafaultError = 500;
 
 module.exports = {
-  ERROR_CODE, NOT_FOUND, ERROR_DEFAULT,
+  ErrorCode, NotFound, DeafaultError,
 };
