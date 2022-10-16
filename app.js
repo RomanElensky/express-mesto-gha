02cc76d4c75d2122045cb68d2879d725359d3437
Roman Elensky <warrior.394@yandex.ru>
2022-10-17 03:19:24 +0400

    4

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
-
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
+
 const { NotFound } = require('./utils/errors');
 
 const { PORT = 3000 } = process.env;
@@ -15,22 +15,22 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', routerUsers);
-app.use('/cards', routerCards);
-
-app.use('*', (req, res) => {
-  res.status(NotFound).send({ message: 'Страница не найдена' });
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
   req.user = {
     _id: '634a9f09b6c26ed5c1bfbc26',
   };
+
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
+app.use('*', (req, res) => {
+  res.status(NotFound).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
