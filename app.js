c0e5fb2bb53d27454f38f1f787dd6395c2f05d95
Roman Elensky <warrior.394@yandex.ru>
2022-10-24 22:19:29 +0400

    24 3

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
 const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-const { default: mongoose } = require('mongoose');
-
-const { PORT = 3000 } = process.env;
-const app = express();
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 
-const auth = require('./middlewares/auth');
+const bodyParser = require('body-parser');
+
+const { login, postUser } = require('./controllers/users');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
-const { NotFound } = require('./utils/errors');
-const { login, postUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const { validatePostUser, validateLogin } = require('./middlewares/validators');
+const { NotFound } = require('./utils/errors');
+
+const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
 app.post('/signin', validateLogin, login);
 app.post('/signup', validatePostUser, postUser);
-
-app.use(auth);
-
-app.use('/users', routerUsers);
-app.use('/cards', routerCards);
-app.use('*', () => {
-  throw new NotFound('Страница не найдена');
-});
-
-app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
@@ -43,6 +33,18 @@ app.use((err, req, res, next) => {
   next();
 });
 
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
+app.use('*', (req, res) => {
+  res.status(NotFound).send({ message: 'Страница не найдена' });
+});
+app.use(errors());
+app.use(auth);
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
 app.listen(PORT, () => {
-  console.log('all is right');
+  console.log(`Подключен к порту ${PORT}`);
 });
