97b4eb49a1b62d0b05a2654295eec03404cb2c88
Roman Elensky <warrior.394@yandex.ru>
2022-10-25 05:16:48 +0400

    25 3

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-
-const app = express();
-
-const mongoose = require('mongoose');
-
 require('dotenv').config();
 
-const { errors } = require('celebrate');
-
-const bodyParser = require('body-parser');
-
-const { login, postUser } = require('./controllers/users');
-const routerUsers = require('./routes/users');
-const routerCards = require('./routes/cards');
-const auth = require('./middlewares/auth');
-const { validatePostUser, validateLogin } = require('./middlewares/validators');
-const { NotFound } = require('./utils/errors');
+const { default: mongoose } = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+const app = express();
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+const auth = require('./middlewares/auth');
+const routerUsers = require('./routes/users');
+const routerCards = require('./routes/cards');
+const { NotFound } = require('./utils/errors');
+const { login, postUser } = require('./controllers/users');
+const { validatePostUser, validateLogin } = require('./middlewares/validators');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validatePostUser, postUser);
@@ -29,9 +29,10 @@ app.use(auth);
 
 app.use('/users', routerUsers);
 app.use('/cards', routerCards);
-app.use('*', (req, res) => {
-  res.status(NotFound).send({ message: 'Страница не найдена' });
+app.use('*', () => {
+  throw new NotFound('Страница не найдена');
 });
+
 app.use(errors());
 
 app.use((err, req, res, next) => {
@@ -42,10 +43,6 @@ app.use((err, req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
 app.listen(PORT, () => {
-  console.log(`Подключен к порту ${PORT}`);
+  console.log('all is right');
 });
